1b77a0263341028a6cd63c44499f25766c1c1f56
GiSuk-Bang <bang981024@gmail.com>
2023-04-16 21:25:43 +0900

    homework

---
+++ Week6/Week6/header1.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//정수 자료형을 파라미터로 하는 최대 공약수 함수 
+int max1(int a, int b);
+//파라미터가 없는 최대 공약수 함수
+void max2(void);
+
+//정수 자료형을 파라미터로 하는 최소 공배수 함수
+int min1(int a, int b);
+//파라미터가 없는 최소 공배수 함수
+void min2(void);
+
+//정수 자료형을 파라미터로 하는 소수 판별 함수
+int prime_number1(int num);
+//파라미터가 없는 소수 판별 함수
+void prime_number2(void);
